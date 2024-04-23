0a6d8a3a14a955df2b7fc3acb2f06f7b2ee42fcf
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-23 22:59:33 +0200

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -47,14 +47,21 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs: Dict[str, str]) -> User:
+        """Find a user by specified attributes.
+
+        Raises:
+            error: NoResultFound: When no results are found.
+            error: InvalidRequestError: When invalid query arguments are passed
+
+        Returns:
+            User: First row found in the `users` table.
         """
-        Find user
-        """
-        sesssion = self._session
+        session = self._session
         try:
-            quary = sesssion.query(User).filter_by(**kwargs).one()
+            user = session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
             raise NoResultFound()
         except InvalidRequestError:
             raise InvalidRequestError()
-        return quary
+        # print("Type of user: {}".format(type(user)))
+        return user
