6e232a8326b9be87e12d80e0f9d6922591a46349
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-23 23:06:25 +0200

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """db module"""
-from sqlalchemy import create_engine, tuple_
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from typing import Dict
 
 from user import Base, User
@@ -48,18 +47,13 @@ class DB:
 
     def find_user_by(self, **kwargs: Dict[str, str]) -> User:
         """
-        find user
+        Find user
         """
-        attrs, vals = [], []
-        for attr, val in kwargs.items():
-            if not hasattr(User, attr):
-                raise InvalidRequestError()
-            attrs.append(getattr(User, attr))
-            vals.append(val)
-
-        session = self._session
-        query = session.query(User)
-        user = query.filter(tuple_(*attrs).in_([tuple(vals)])).first()
-        if not user:
-            raise NoResultFound()
-        return user
+        sesssion = self._session
+        try:
+            quary = sesssion.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+        return quary
